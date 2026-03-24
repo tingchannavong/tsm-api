175a1b296ec4daf638949417390d251a0d92e968
tingchannavong <tingchannavong.gmail.com>
2026-03-24 11:07:41 +0700

    added global errorhandler for unique constraint db level

--- src/middlewares/errorHandler.middleware.js
+++ src/middlewares/errorHandler.middleware.js
@@ -1,9 +1,26 @@
+import { Prisma } from "@prisma/client";
+
 // special error handler middleware with error 1st argument
 // have to have total arg 4
 export default function errorHandler(err, re, res, next) {
-    const status = err.status || 500;
-    res.status(status).json({
+  console.dir(err)
+  const status = err.status || 500;
+
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    const uniqueConstraintRegex = /_(\w*)_key/;
+    const match = err.message.match(uniqueConstraintRegex);
+
+    if (match) {
+      const field = match[1];
+      return res.status(status).json({
         success: false,
-        message: err.message || "Internal server error"
-    })
+        message: `${field} is already taken.`,
+      });
+    }
+  }
+
+  res.status(status).json({
+    success: false,
+    message: err.message || "Internal server error",
+  });
 }
