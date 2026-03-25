f75eb4a8615c49e1f28448ddc0bfc40ab6b95d83
tingchannavong <tingchannavong.gmail.com>
2026-03-25 11:05:10 +0700

    move business logic from controller to service

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -1,5 +1,5 @@
 import {
-  createSession,
+  createSessions,
   deleteSessionById,
   getAllSessions,
   getSessionById,
@@ -8,56 +8,16 @@ import {
 } from "../services/session.service.js";
 import createError from "http-errors";
 
-export async function createSessionController(req, res, next) {
-  const { locationId, groupId, people, name1, pricingId } = req.body;
-
-  const data = {};
-  const responses = [];
-  var newGroupId = "";
-
-  // TO ADD: auto-generate names feature
-
+export async function createSessionsController(req, res, next) {
   try {
-    // Create person 1 session first
-    if (name1) data.name = name1;
-    if (locationId) data.locationId = locationId;
-    if (groupId) data.groupId = groupId;
-    if (pricingId) data.pricingId = pricingId;
-
-    const resp = await createSession(data);
-    newGroupId = resp.groupId;
-    responses.push(resp);
-  } catch (error) {
-    next(error);
-  }
-
-  // For group, get group id from response, create the rest
-  if (people > 1) {
-    const data = {};
-    const ppl = Number(people);
-
-    // start at person number 2
-    for (var i = 2; i < ppl + 1; i++) {
-      const eachName = req.body[`name${i}`];
-      data.groupId = newGroupId;
-
-      if (eachName) data.name = eachName;
-      if (locationId) data.locationId = locationId;
-      if (pricingId) data.pricingId = pricingId;
-
-      try {
-        const resp = await createSession(data);
-        responses.push(resp);
-      } catch (error) {
-        next(error);
-      }
-    }
-  }
-
-  res.status(201).json({
-    message: "Sessions created successfully",
+    const responses = await createSessions(req.body);
+    res.status(201).json({
+    message: "Session(s) created successfully",
     responses,
   });
+  } catch (error) {
+    next(error)
+  }
 }
 
 export async function getFilteredSessionsController(req, res, next) {
--- src/services/session.service.js
+++ src/services/session.service.js
@@ -1,8 +1,54 @@
 import prisma from "../libs/prismaClient.js";
 
-export async function createSession(data) {
+export async function createSessions(payload) {
+  const {
+    name1 = "Guest 1",
+    locationId = undefined,
+    groupId = undefined,
+    people = undefined,
+    pricingId = undefined,
+  } = payload;
+
+  const totalPeople = Number(people) || 1;
+  const result = [];
+
+  // TO ADD: auto-generate names feature, inclu name1
+
+  // Create first session
+  const first = await createSession({
+    name: name1,
+    locationId,
+    groupId,
+    pricingId,
+  });
+
+  const newGroupId = first.groupId;
+  result.push(first);
+
+  // For group, get group id from response, create the rest
+  for (let i = 2; i <= totalPeople; i++) {
+    const eachName = payload[`name${i}`];
+
+    const session = await createSession({
+      name: eachName,
+      locationId: locationId,
+      groupId: newGroupId,
+      pricingId,
+    });
+    result.push(session);
+  }
+
+  return result;
+}
+
+export async function createSession(sessionData) {
   const result = await prisma.sessionRecord.create({
-    data,
+    data: {
+      name: sessionData.name,
+      locationId: sessionData.locationId, 
+      groupId: sessionData.groupId,
+      pricingId: sessionData.pricingId,
+    },
   });
 
   return result;
@@ -21,27 +67,27 @@ export async function getAllSessions(filters) {
     return await prisma.sessionRecord.findMany();
   }
   const result = await prisma.sessionRecord.findMany({
-    where: filters
+    where: filters,
   });
 
   return result;
 }
 
 export async function getSessionById(id) {
-    return await prisma.sessionRecord.findUnique({
-        where: {id}
-    });
+  return await prisma.sessionRecord.findUnique({
+    where: { id },
+  });
 }
 
 export async function updateSessionById(id, data) {
-    return await prisma.sessionRecord.update({
-        where: {id},
-        data
-    });
+  return await prisma.sessionRecord.update({
+    where: { id },
+    data,
+  });
 }
 
 export async function deleteSessionById(id) {
-    return await prisma.sessionRecord.delete({
-        where: {id}
-    });
+  return await prisma.sessionRecord.delete({
+    where: { id },
+  });
 }
