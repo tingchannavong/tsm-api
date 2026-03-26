cf41e45ddf1be980b3d5e6515e5554486dcca997
tingchannavong <tingchannavong.gmail.com>
2026-03-26 14:29:13 +0700

    change validate to object assign otherwise original info gets write over

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -9,18 +9,23 @@ export const validate = (schema) => (req, res, next) => {
       body: req.body,
     });
 
-    if (validated.params) req.params = validated.params;
+    if (validated.params) Object.assign(req.params, validated.params);
     if (validated.query) Object.assign(req.query, validated.query);
-    if (validated.body) req.body = validated.body;
+    if (validated.body) Object.assign(req.body, validated.body);
 
     next();
   } catch (error) {
     // console.log(error.flatten().fieldErrors);
     // Show zod error message
     if (error.issues && error.issues.length > 0) {
-      next(createError(400, `${error.issues[0].message} at ${error.issues[0].path}`));
+      next(
+        createError(
+          400,
+          `${error.issues[0].message} at ${error.issues[0].path}`,
+        ),
+      );
     }
-    
+
     next(error);
   }
 };
