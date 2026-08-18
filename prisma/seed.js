27acfe1eb5f4ddd6a16d94f7cf28a3362f2d28c5
tingchannavong <tingchannavong.gmail.com>
2026-08-18 21:18:00 +0700

    feat: update seed script to include location seeding and modify start script to run seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,17 +1,70 @@
+console.log("SCRIPT STARTED");
+
 import prisma from "../src/libs/prismaClient.js";
+import bcrypt from "bcrypt";
 
 async function main() {
-  await prisma.sessionRecord.deleteMany({ where: { status: {in: ['ACTIVE', 'ENDED'] }} });  
-  //  await prisma.pricing.create({
-  //   data: {
-  //     billingIntervalMin: 1,
-  //     name: "Hourly",
-  //     currencyId: 1,
-  //     unitId: 1,
-  //     roundingMethod: "ROUND_UP",
-  //     price: 1.23
+  // await prisma.sessionRecord.deleteMany({ where: { status: {in: ['ACTIVE', 'ENDED'] }} });  
+   await prisma.pricing.create({
+    data: {
+      billingIntervalMin: 1,
+      name: "Hourly",
+      currencyId: 1,
+      unitId: 1,
+      roundingMethod: "ROUND_UP",
+      price: 1.23
+    },
+  });
+
+   for (let i = 1; i <= 6; i++) {
+    const tableName = `Table-${i}`;
+    const tableDisplayName = `T${i}`;
+
+    const location = await prisma.location.upsert({
+      where: { name: tableName },
+      // update: {
+      //   displayName: tableDisplayName,
+      // },
+      create: {
+        name: tableName,
+        displayName: tableDisplayName,
+        qrCode: `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${tableName}`, // Optional dummy QR code link
+      },
+    });
+
+    console.log(`Seeded: ${location.displayName} (${location.id})`);
+  }
+
+  // const username = 'admin';
+  // const plainPassword = 'Superhero111'; 
+  // const hashedPassword = await bcrypt.hash(plainPassword, 10);
+
+  // console.log('Seeding superadmin user...');
+
+  // const superAdmin = await prisma.user.upsert({
+  //   where: { username: username },
+  //   update: {}, // Do nothing if the user already exists
+  //   create: {
+  //     username: username,
+  //     password: hashedPassword,
+  //     email: 'superadmin@example.com',
+  //     firstname: 'Super',
+  //     lastname: 'Admin',
+  //     phone: '+1234567890',
+  //     provider: 'local',
+  //     role: 'ADMIN', 
+  //     status: 'ACTIVE',
   //   },
-  // })
+  // });
+
+  // console.log(`Superadmin created/verified successfully:`);
+  // console.log({
+  //   id: superAdmin.id,
+  //   username: superAdmin.username,
+  //   email: superAdmin.email,
+  //   role: superAdmin.role,
+  // });
+
 }
 
 main().catch(error => {
